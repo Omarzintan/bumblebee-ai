e6d2c886fbaae35149301134f66afc139bbfec61
Omarzintan <zintanomar@gmail.com>
2021-01-14 00:22:13 -0800

    add chrome extension folder

---
+++ extension/background.js
@@ -0,0 +1,61 @@
+// Listener for when a new tab is activated
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function (tab) {
+        y = tab.url;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+            }
+        };
+        xhttp.open("POST", "http://127.0.0.1:5000/send_url");
+        xhttp.send("url=" + y);
+    });
+});
+
+// Listener for when user switches between tabs
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+    if (tab.active && change.url) {
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 & this.status == 200) {
+                console.log(this.responseText);
+            }
+        };
+        xhttp.open("POST", "http://127.0.0.1:5000/send_url");
+        xhttp.send("url=" + change.url);
+    }
+});
+
+// Detecting when a tab is closed.
+
+// Define a mapping between tabId and url:
+var tabToUrl = {};
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab)
+{
+    //store tabId and tab url as key value pair:
+    tabToUrl[tabId] = tab.url;
+
+});
+
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    // since tab is not available insice onRemoved,
+    // we have to use the mapping we created above to get the removed tab url:
+    console.log(tabToUrl[tabId]);
+
+    var xhttp2 = new XMLHttpRequest();
+    xhttp2.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp2.open("POST", "http://127.0.0.1:5000/quit_url");
+    xhttp2.send("url=" + tabToUrl[tabId]);
+
+    // Remove information for the now non-existent tab
+    delete tabToUrl[tabId];
+});
+
+
