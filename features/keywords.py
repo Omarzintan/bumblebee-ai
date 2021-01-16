d7cc284768dde420be492df24d8abf2aa4ba6f0b
Omarzintan <zintanomar@gmail.com>
2021-01-15 23:13:45 -0800

    add keywords.py

---
+++ features/keywords.py
@@ -0,0 +1,27 @@
+class Keywords:
+    """Dictionary containing all keywords to corresponding functions in bumblebee.py
+    """
+    def __init__(self):
+        self.keywords = {
+            "sleep": ['i\'m done', 'all done', 'exit', 'bye', 'go to sleep'],
+            "search_wikipedia": ['wikipedia'],
+            "search_wolframalpha": ['calculate', 'what is'],
+            "search_google": ['google'],
+            "search_youtube": ['youtube'],
+            "time": ['time'],
+            "greet": ['hello', 'what\'s up', 'hey', 'yo'],
+            "help": ['about yourself', 'help me', 'who are you', 'what are you'],
+            "notepad": ['open notepad'],
+            "send_email": ['send email'],
+            "add_contact": ['add email contact', 'add contact'],
+            "stop_research": ['stop research', 'exit research mode', 'done researching'],
+            "start_research": ['start research', 'research mode' 'let\'s do research'],
+            "store_research_data": ['store data', 'save research', 'save tabs'],
+            "open_shell": ['open shell'],
+            "clock_in": ['clock in', 'let\'s work', 'start work'],
+            "clock_out": ['clock out', 'done working', 'stop work'],
+            "stop_listening": ['stop listening', 'shutdown'],
+        }
+
+    def get_keywords(self, key_name):
+        return self.keywords[key_name]
