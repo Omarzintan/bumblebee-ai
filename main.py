15ab335404d33eecdf35f654d80a1a4d4fc0fcab
Omarzintan <zintanomar@gmail.com>
2021-03-21 19:07:18 -0700

    make retraining happen everytime intents.json is updated

--- main.py
+++ main.py
@@ -3,31 +3,42 @@ import features
 import sys, os
 import json
 import importlib
+import train
 
 if __name__ == "__main__":
-    
-    print(features.__all__)
-    
-    # Update intents.json if features have been added/removed
-    with open('features/intents.json', 'r') as json_data:
-        intents = json.load(json_data)
-        
-    if len(features.__all__) != len(intents['intents']):
+    # Check to see that intents.json file exists.
+    try:
+        with open('features/intents.json', 'r') as json_data:
+            intents = json.load(json_data)
+        assert(len(features.__all__) == len(intents['intents']))
+    except:
+        # remove file if it exists        
+        try:
+            os.remove('features/intents.json')
+        except:
+            print('intents.json file not found.')
+            
+        # Update intents.json if features have been added/removed or the file does not exist.        
         print('Generating new intents.json file...')
-        os.remove('features/intents.json')
+
         intents = {}
         intents['intents'] = []
-        for feature in features.__all__:
+        for x, feature in enumerate(features.__all__):
             feature_object = importlib.import_module('features.'+feature, ".").Feature()
             tag = {}
             tag["tag"] = feature_object.tag_name
             tag["patterns"] = feature_object.patterns
+            tag["index"] = x
             intents['intents'].append(tag)
 
         intents_json = json.dumps(intents, indent=4)
         
         with open('features/intents.json', 'w') as f:
             f.write(intents_json)
+
+        # Retrain the NeuralNet
+        print("Retraining NeuralNet...")
+        train.train()
         
     bumblebee = Bumblebee(features.__all__)
     bumblebee.run()
