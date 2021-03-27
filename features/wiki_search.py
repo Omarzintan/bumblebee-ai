45cb1536b09a91001ff183a4bfebac8540756fb1
Omarzintan <zintanomar@gmail.com>
2021-03-26 21:43:11 -0700

    update get search query functions for these features

--- features/wiki_search.py
+++ features/wiki_search.py
@@ -5,13 +5,13 @@ import sys
 class Feature(BaseFeature):
     def __init__(self):
         self.tag_name = "wiki_search"
-        self.patterns = ["wikipedia"]
+        self.patterns = ["wikipedia", "search wikipedia for", "look up on wikipedia"]
         super().__init__()
 
     def action(self, spoken_text):
-        search_query = self.get_search_query(spoken_text, self.keywords)
+        search_query = self.get_search_query(spoken_text, self.patterns)
         try:
-            results = wikipedia.summary(search_query, sentences = 3)
+            results = wikipedia.summary(search_query, sentences = 2)
             self.bs.respond('According to Wikipedia')
             self.bs.respond(results)
             return
@@ -21,10 +21,26 @@ class Feature(BaseFeature):
 
     '''
     Parses spoken text to retrieve a search query for Wikipedia
-    Argument: <string> spoken_text, <list> keywords
+    Argument: <string> spoken_text (tokenized. i.e. list of words), <list> patterns
     Return type: <string> spoken_text (this is actually the search query as retrieved from spoken_text.
     '''
-    def get_search_query(self, spoken_text, keywords):
-        for word in keywords:
-            spoken_text = spoken_text.replace(word, '')
-        return spoken_text
+    def get_search_query(self, spoken_text, patterns):
+        search_terms = ['about', 'on', 'for', 'search']
+        query_found = False
+
+        for search_term in search_terms:
+            if search_term in spoken_text:
+                search_index = spoken_text.index(search_term)
+                # get everything after the search term
+                spoken_text = spoken_text[search_index+1:]
+                query_found = True
+                break
+        
+        if not query_found:
+            for phrase in patterns:
+                # split the phrase into individual words
+                phrase_list = phrase.split(' ')
+                # remove phrase list from spoken_text
+                spoken_text = [word for word in spoken_text if word not in phrase_list]
+
+        return ' '.join(spoken_text)
--- features/youtube_search.py
+++ features/youtube_search.py
@@ -9,28 +9,43 @@ class Feature(BaseFeature):
         super().__init__()
 
     def action(self, spoken_text):
-        query = self.search(spoken_text, self.keywords)
+        query = self.search(spoken_text)
         self.bs.respond('I have opened YouTube with a search on {}'.format(query)) 
         return
 
-
-
     '''
-    Parses spoken text to retrieve a search query for YouTube
-    Argument: <string> spoken_text, <list> keywords
-    Return type: <string> spoken_text (this is actually the search query as retrieved from spoken_text.
+    Parses spoken text to retrieve a search query for Youtube
+    Argument: <list> spoken_text (tokenized. i.e. list of words), <list> patterns
+    Return type: <string> spoken_text (this is actually the search query as retrieved from spoken_text.)
     '''
-    def get_search_query(self, spoken_text, keywords):
-        for word in keywords:
-            spoken_text = spoken_text.replace(word, '')
-        return spoken_text
+    def get_search_query(self, spoken_text, patterns):
+        search_terms = ['about', 'on', 'for', 'search']
+        query_found = False
+
+        for search_term in search_terms:
+            if search_term in spoken_text:
+                search_index = spoken_text.index(search_term)
+                # get everything after the search term
+                spoken_text = spoken_text[search_index+1:]
+                query_found = True
+                break
+
+        # In case none of the search terms are included in spoken_text.
+        if not query_found:
+            for phrase in patterns:
+                # split the phrase into individual words
+                phrase_list = phrase.split(' ')
+                # remove phrase list from spoken_text
+                spoken_text = [word for word in spoken_text if word not in phrase_list]
+                
+        return ' '.join(spoken_text)
 
     '''
     Opens YouTube in a browser with the specified search query.
     Argument: <string> spoken_text, <list> keywords
     Return type: <string> query
     '''
-    def search(self, spoken_text, keywords):
-        query = get_search_query(spoken_text, keywords)
+    def search(self, spoken_text):
+        query = self.get_search_query(spoken_text, self.patterns)
         webbrowser.open("https://www.youtube.com/results?search_query='{}'".format(query))
         return query
