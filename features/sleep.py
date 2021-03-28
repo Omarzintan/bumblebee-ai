9ef9e80070efeab23038f7cd6fe7a311201a3d5d
Omarzintan <zintanomar@gmail.com>
2021-03-28 02:00:30 -0700

    fix bug in sleep feature

--- features/sleep.py
+++ features/sleep.py
@@ -5,7 +5,8 @@ class Feature(BaseFeature):
     def __init__(self):
         self.tag_name = "sleep"
         self.patterns = ["I am done", "all done", "exit", "go to sleep", "bye"]
-
+        super().__init__()
+        
     def action(self, spoken_text=''):
         self.bs.respond('Ok. I\'ll be listening for your command.')
         Bumblebee.sleep = 1
