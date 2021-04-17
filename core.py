0ea9492d5ed123c0dcde99d4bde3b8fbde5080ed
Omarzintan <zintanomar@gmail.com>
2021-04-17 16:28:13 -0700

    add failsafe functions for threads:

--- core.py
+++ core.py
@@ -1,15 +1,14 @@
 '''The core of Bumblebee.'''
 import importlib
 import os
-import sys
 import json
-import pickle
 import subprocess
 import torch
 
 from utils.speech import BumbleSpeech
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
+
 
 class Bumblebee():
     # global vars
@@ -22,7 +21,7 @@ class Bumblebee():
         "work_start_time": '',
         "research_server_proc": '',
         "research_topic": '',
-        "threads": {}
+        "threads": []
         }
 
     def __init__(self, features: list = ['default'], config: dict = {}):
@@ -58,11 +57,11 @@ class Bumblebee():
                 intents = json.load(json_data)
             # Check whether any features have been added/removed.
             assert(len(self._features) == len(intents['intents']))
-        except:
+        except (FileNotFoundError, AssertionError):
             # remove intents file if it exists
             try:
                 os.remove(self.bumblebee_dir+'utils/intents.json')
-            except:
+            except OSError:
                 print('intents.json file not found.')
             
             # Update intents.json if features have been added/removed or the file does not exist.
@@ -157,50 +156,3 @@ class Bumblebee():
     def get_config(self):
         '''Get the config file that Bumblebee is running with.'''
         return self.config_yaml
-
-    """
-    FUNCTIONS NECESSARY FOR CRASH RECOVERY
-    Crash recovery entails storing desired global variables in case
-    a crash happens. So far, the employee info is stored and preserved 
-    if the user is currently working. On reboot, the information about 
-    much time has been spent working is restored. Also, if the research
-    server process is running, it is killed before reboot happens.
-    """
-    def store_vars(self):
-        Bumblebee.crash_store['work_start_time'] = Bumblebee.work_start_time
-        Bumblebee.crash_store['currently_working'] = Bumblebee.currently_working
-        Bumblebee.crash_store['employer'] = Bumblebee.employer
-        with open(Bumblebee.crash_file, 'wb') as f:
-            f.seek(0)
-            pickle.dump(Bumblebee.crash_store, f)
-        return
-        
-    def restore_vars(self):
-        Bumblebee.crash_store = pickle.load(open(Bumblebee.crash_file, "rb"))
-        Bumblebee.work_start_time = Bumblebee.crash_store['work_start_time']
-        Bumblebee.currently_working = Bumblebee.crash_store['currently_working']
-        Bumblebee.employer = Bumblebee.crash_store['employer']
-        return
-
-    def start_gracefully(self):
-        try:
-            if os.path.exists(Bumblebee.crash_file):
-                print('Starting gracefully.')
-                self.restore_vars()
-                os.remove(Bumblebee.crash_file)
-        except:
-            print(sys.exc_info())
-            print('Start gracefully failed.')
-            pass
-        return
-
-    def exit_gracefully(self):
-        print('Exiting gracefully.')
-        if Bumblebee.research_server_proc:
-            Bumblebee.speech.respond('Closing research server gracefully.')
-            store_research_feature_index = self.feature_indices['store_research_data']
-            stop_research_feature_index = self.feature_indices['stop_research_data']
-            self._features[store_research_feature_index].action()
-            self._features[stop_research_feature_index].action()
-        if Bumblebee.currently_working:
-            self.store_vars()
--- utils/run_gracefully.py
+++ utils/run_gracefully.py
@@ -1,52 +1,64 @@
 """
-FUNCTIONS NECESSARY FOR CRASH RECOVERY
-Crash recovery entails storing desired global variables in case
-a crash happens. So far, the employee info is stored and preserved
-if the user is currently working. On reboot, the information about
-much time has been spent working is restored. Also, if the research
-server process is running, it is killed before reboot happens.
+Contains functions necessary for graceful running of Bumblebee.
+This entails storing global variables in case of a crash and restoring them
+on reboot.
+This also includes stopping all running subprocesses before exiting Bumblebee.
 """
+import os
+import pickle
+from core import Bumblebee
+from utils import wake_word_detector
+from utils.globals_api import GLOBALSAPI
+
+
+CRASH_FILE = 'crash_recovery.p'
+globals_api = GLOBALSAPI()
 
 
 def store_vars():
     '''Stores variables in a pickle file.'''
-    with open(crash_file, 'wb') as f:
+    with open(CRASH_FILE, 'wb') as f:
         f.seek(0)
         pickle.dump(Bumblebee.global_store, f)
-    return
 
 
 def restore_vars():
     '''Restores pickled variables.'''
-    Bumblebee.global_store = pickle.load(open(crash_file, "rb"))
-    return
+    Bumblebee.global_store = pickle.load(open(CRASH_FILE, "rb"))
 
 
 def start_gracefully():
     '''Restores stored global vars from before crash happened.'''
     try:
-        if os.path.exists(crash_file):
+        if os.path.exists(CRASH_FILE):
             print('Starting gracefully.')
             restore_vars()
-            os.remove(crash_file)
-    except:
-        print(sys.exc_info())
+            os.remove(CRASH_FILE)
+    except OSError as exception:
+        print(exception)
         print('Start gracefully failed.')
-        pass
-    return
 
 
-# FIX THIS
-# check for any running processes and end them?
-# 
-def exit_gracefully():
+def exit_gracefully(bumblebee, crash_happened=False):
+    '''
+    Exiting gracefully means checking for any running threads
+    and terminating them as well as storing global variables if
+    a crash happend.
+    Arguments: <Bumblebee> bumblebee (an instance of Bumblebee),
+               <boolean> crash_happened
+    Returns: None
+    '''
     print('Exiting gracefully.')
-    if retrieve("research_server_proc") != "":
-        Bumblebee.speech.respond('Closing research server gracefully.')
-        store_research_feature_index = .feature_indices['store_research_data']
-        stop_research_feature_index = .feature_indices['stop_research_data']
-        ._features[store_research_feature_index].action()
-        _features[stop_research_feature_index].action()
-    if Bumblebee.currently_working:
-        store_vars()    
+    # If a crash happened we store all global vars
+    # includeing the proccess ids for all running
+    # threads.
+    if crash_happened:
+        store_vars()
+        return
 
+    # If this is a regular exiting, we ensure all threads are
+    # terminated before we exit.
+    threads = globals_api.retrieve("threads")
+    for thread in threads:
+        bumblebee.run_by_tags(thread["terminate"])
+    wake_word_detector.stop()
