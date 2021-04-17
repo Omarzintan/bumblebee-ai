e46216355e4cf73e0378d04902870d8b730ec1ef
Omarzintan <zintanomar@gmail.com>
2021-04-17 16:29:48 -0700

    revert helpers.py to what it used to be

--- helpers.py
+++ helpers.py
@@ -2,6 +2,8 @@ import os
 import logging
 from colorlog import ColoredFormatter
 
+
+
 def get_logger(name, with_formatting=True, dest=None, format_string=None, log_level=logging.DEBUG):
     '''Get logger with specific format string. Raises exception if error occurs.
     Sources: https://www.machinelearningplus.com/python/python-logging-guide/, 
@@ -16,23 +18,29 @@ def get_logger(name, with_formatting=True, dest=None, format_string=None, log_le
     try:
         handler = logging.FileHandler(dest) if dest else logging.StreamHandler() # default to console
     except Exception as e:
-       raise e
+        raise e
 
     if with_formatting or format_string:
         log_format = format_string if format_string else default_log_format
         formatter = ColoredFormatter(log_format) # create formatter
         handler.setFormatter(formatter) # set formatter
-        
+
     logger.addHandler(handler)
     return logger
 
-logger  = get_logger(__name__)
+
+logger = get_logger(__name__)
+
 
 def get_root_directory():
     '''Gets the root directory of the project. Source: https://www.kite.com/python/answers/how-to-get-the-path-of-the-root-project-structure-in-python'''
-    top_level_filename = "main.py"
-    bumblebee_root_dir = os.path.dirname(os.path.abspath(top_level_filename))
-        
-    return bumblebee_root_dir + "/" 
+    bumblebee_root_dir = os.getenv('BUMBLEBEE_PATH')
+    if not bumblebee_root_dir:
+        logger.debug("No BUMBLEBEE_PATH is set in environment. Computing root directory instead.")
+        top_level_filename = "main.py"
+        bumblebee_root_dir = os.path.dirname(os.path.abspath(top_level_filename))
+
+    return bumblebee_root_dir + "/"
+
 
 bumblebee_root = get_root_directory()
