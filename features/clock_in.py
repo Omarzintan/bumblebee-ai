c281c4a7e265417bab4037e93ee04ca85bcaafd6
Rexford Essilfie <rexfordessilfie09@gmail.com>
2021-04-25 03:54:46 -0500

    use globals_api in clock_in

--- features/clock_in.py
+++ features/clock_in.py
@@ -1,10 +1,18 @@
 from features.default import BaseFeature
-from core import Bumblebee
 import difflib
 import datetime
 import os
 from tinydb import TinyDB
 from helpers import bumblebee_root
+
+
+# TODO:  went back and forth between naming this StoreKeys, or just Constants, but I think I prefer Constants
+class Constants:
+    # Store Keys
+    EMPLOYER = 'employer'
+    WORK_START_TIME = 'work_start_time'
+    DATETIME = 'datetime'
+    CURRENTLY_WORKING = 'currently_working'
 
 
 class Feature(BaseFeature):
@@ -14,34 +22,54 @@ class Feature(BaseFeature):
         super().__init__()
 
     def action(self, spoken_text):
-        employers = self.get_employers()
+        known_employers = self.get_employers()
         self.bs.respond('Which employer is this for?')
-        print(f'List of employers: {employers}')
-        Bumblebee.employer = ''
-        Bumblebee.employer = self.bs.infinite_speaking_chances(Bumblebee.employer)
-        if self.bs.interrupt_check(Bumblebee.employer):
+        print(f'List of employers: {known_employers}')
+
+        self.globals_api.store(Constants.EMPLOYER, '')
+
+        employer_text = self.bs.infinite_speaking_chances()
+
+        if self.bs.interrupt_check(employer_text):
             return
+
         close_names = []
         while close_names == []:
-            close_names = difflib.get_close_matches(Bumblebee.employer, employers)
+            close_names = difflib.get_close_matches(
+                employer_text, known_employers)
             if close_names == []:
-                self.bs.respond('I don\'t know this employer. Please try again')
-                Bumblebee.employer = ''
-                Bumblebee.employer = self.bs.infinite_speaking_chances(Bumblebee.employer)
-                if self.bs.interrupt_check(Bumblebee.employer):
+                self.bs.respond(
+                    'I don\'t know this employer. Please try again')
+
+                self.globals_api.store(Constants.EMPLOYER, '')
+
+                employer_text = self.bs.infinite_speaking_chances(
+                    employer_text)
+
+                if self.bs.interrupt_check(employer_text):
                     break
 
-        Bumblebee.employer = close_names[0]
-        for employer in employers:
-            if employer in Bumblebee.employer:
-                Bumblebee.employer = employer
-                Bumblebee.work_start_time = datetime.datetime.now()
-                Bumblebee.currently_working = True
+        # TODO: Is this step correct? Is close_names a list of lists?
+        closest_matches = close_names[0]
+
+        for employer in known_employers:
+            # TODO: again this seems like it only makes sense if close_names is a list of lists
+            if employer in closest_matches:
+                # TODO: put this into one object. Not sure if there is a strong reason for them to be separate
+                self.globals_api.store(Constants.EMPLOYER, employer)
+                self.globals_api.store(
+                    Constants.WORK_START_TIME, datetime.datetime.now())
+                self.globals_api.store(Constants.CURRENTLY_WORKING, True)
                 # access peggy file and put timestamp there
-                self.clock_in(Bumblebee.employer, Bumblebee.work_start_time.strftime('%a %b %d, %Y %I:%M %p'))
+                self.clock_in(
+                    self.globals_api.retrieve(Constants.EMPLOYER),
+                    self.globals_api.retrieve(Constants.WORK_START_TIME).strftime(
+                        '%a %b %d, %Y %I:%M %p')
+                )
                 break
-            
-        self.bs.respond('You\'ve been clocked in for {}.'.format(Bumblebee.employer))
+
+        self.bs.respond(
+            'You\'ve been clocked in for {}.'.format(self.globals_api.retrieve(Constants.EMPLOYER)))
         return
 
     '''
@@ -49,6 +77,7 @@ class Feature(BaseFeature):
     Arguments: <string> employer name, <datetime.datetime object> work_start_time
     Return type: None
     '''
+
     def clock_in(self, employer, work_start_time):
         # find/create employer file
         os.makedirs('work_study', exist_ok=True)
@@ -58,6 +87,7 @@ class Feature(BaseFeature):
     '''
     Gets a list of all employers from the employer database.
     '''
+
     def get_employers(self):
         employer_db_path = self.config["Database"]["employers"]
         employer_db = TinyDB(employer_db_path)
