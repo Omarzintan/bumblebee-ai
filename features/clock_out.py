dd5643e8c69b8d8101b27d61333f3c42d6b406f8
Rexford Essilfie <rexfordessilfie09@gmail.com>
2021-05-01 02:03:36 -0500

    use globals api in clock_out feature

--- features/clock_out.py
+++ features/clock_out.py
@@ -4,22 +4,38 @@ import os
 from core import Bumblebee
 from helpers import bumblebee_root
 
+from features.clock_in import StoreKeys
+
 
 class Feature(BaseFeature):
     def __init__(self):
         self.tag_name = "clock_out"
-        self.patterns = ["clock out", "done working", "stop work", "clock me out of work"]
+        self.patterns = ["clock out", "done working",
+                         "stop work", "clock me out of work"]
         super().__init__()
 
-    def action(self, spoken_text=''):
-        if not Bumblebee.currently_working:
+    def action(self, spoken_text):
+        is_currently_working = self.globals_api.retrieve(
+            StoreKeys.CURRENTLY_WORKING)
+
+        if not is_currently_working:
             self.bs.respond('You\'ve not been clocked in.')
             return
-        
+
+        work_start_time = self.globals_api.retrieve(StoreKeys.WORK_START_TIME)
         work_stop_time = datetime.datetime.now()
-        duration = (work_stop_time - Bumblebee.work_start_time)
+        duration = (work_stop_time - work_start_time)
+
         print('Duration: ', duration)
-        self.clock_out(Bumblebee.employer, work_stop_time.strftime('%a %b %d, %Y %I:%M %p'), duration)
+        self.clock_out(self.globals_api.retrieve(StoreKeys.EMPLOYER),
+                       work_stop_time.strftime('%a %b %d, %Y %I:%M %p'),
+                       duration)
+
+        # Clear store values related to work
+        self.globals_api.store(StoreKeys.EMPLOYER, '')
+        self.globals_api.store(StoreKeys.CURRENTLY_WORKING, False)
+        self.globals_api.store(StoreKeys.WORK_START_TIME, '')
+
         self.bs.respond('You\'ve been clocked out.')
         return
 
@@ -27,11 +43,11 @@ class Feature(BaseFeature):
     Writes line in employer specific file saying I have logged out of work.
     Arguments: <string> employer name, <datetime.datetime object> work_stop_time, <datetime.timedelta object> duration
     Return type: None
-    '''        
-    def clock_out(employer, work_stop_time, duration):
+    '''
+
+    def clock_out(self, employer, work_stop_time, duration):
         # find/create employer file
         os.makedirs('work_study', exist_ok=True)
         with open(bumblebee_root+os.path.join('work_study', '{}_hours.txt'.format(employer)), 'a+') as file:
             file.write('Ended work: {}\n'.format(work_stop_time))
             file.write('Duration: {}\n'.format(duration))
-
