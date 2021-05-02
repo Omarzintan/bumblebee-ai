0e5cb5f81444f25233e6966d5f9c1b9b2b84014f
Omarzintan <zintanomar@gmail.com>
2021-05-02 12:15:54 -0700

    update bumblebee env

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-conda env create --name bumblebee-ai --file bumblebee_env.yml --force
+conda env create --name bumblebee-ai --file bumblebee_env_mac.yml --force
 conda activate bumblebee-ai
 python main.py
