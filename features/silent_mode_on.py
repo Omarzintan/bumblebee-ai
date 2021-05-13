4674ba92ff4df202dca764ec6b10412cc80ec6e4
Omarzintan <zintanomar@gmail.com>
2021-05-12 18:30:12 -0700

    fix silent mode on feature

--- features/silent_mode_on.py
+++ features/silent_mode_on.py
@@ -10,14 +10,10 @@ class Feature(BaseFeature):
             "turn on silent mode",
             "start silent mode",
         ]
-        super().__init__(bumblebee_api)
+        self.speech = bumblebee_api.get_speech()
 
     def action(self, spoken_text):
-        self.bs.respond('Okay. Starting silent mode.')
-        self.bs.set_speech_mode('silent')
-        self.bs.respond('Welcome to silent mode.')
-        # TODO
-        # use bumblebee_api to exit out of while loop for voice mode,
-        # change the speech mode to silent and then call the run function again
-        # same for switching to voice mode.
+        self.speech.respond('Okay. Starting silent mode.')
+        self.speech.set_speech_mode('silent')
+        self.speech.respond('Welcome to silent mode.')
         return
