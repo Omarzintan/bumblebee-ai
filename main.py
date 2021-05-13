2c4512f103b77986f7e6cecbcdb5e2d3ce8fd69c
Omarzintan <zintanomar@gmail.com>
2021-05-12 22:24:03 -0700

    test all features in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bee_creator import Bumblebee
 
 if __name__ == "__main__":
-    bee_runner = Bumblebee(name="bumblebee", feature_list_name='test')
+    bee_runner = Bumblebee(name="bumblebee", feature_list_name='all')
     bee_runner.run_bee()
