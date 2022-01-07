7b029f95ed81c662f4fbe4e914b9587e9750b745
Rexford Essilfie <rexfordessilfie09@gmail.com>
2022-01-06 22:59:57 -0500

    add env.py and constants.py

---
+++ utils/env.py
@@ -0,0 +1,11 @@
+'''
+This file is for retrieving system environment variables and helper
+variables directly derived from them.
+'''
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+environment = os.environ.get('BUMBLEBEE_ENV')
+is_local = environment == 'local'
